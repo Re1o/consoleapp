fe6d1bd6f1e12b127012f5005c3ea6e1983b969d
Re1o <reio.reimers@hotmail.com>
2019-09-20 18:20:11 +0300

    liigaasta

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearOfBirth;
+
+            Console.WriteLine("Enter your year of birth: ");
+            yearOfBirth = Int32.Parse(Console.ReadLine());
+
+            /*if (yearOfBirth % 4 == 0) 
+            {
+                Console.WriteLine($"{yearOfBirth} was a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine($"{ yearOfBirth} was not a Leap Year");
+            }
+            //Ternary Operator*/
+
+            /*string answer = (yearOfBirth % 4 == 0) ? "Leap Year." : "Not a Leap Year";
+            Console.WriteLine(answer);
+            Console.ReadLine();*/
+
+            Console.WriteLine("What would be your poison? whisky or wine");
+            string userChoise = Console.ReadLine();
+            string choise = userChoise == "whisky" ? "Here is your whisky" : "Here is your wine";
+            Console.WriteLine(choise);
+            Console.WriteLine();
+        }
+
+
+       
+
+    }
+}
